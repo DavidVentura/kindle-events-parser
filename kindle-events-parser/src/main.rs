f7d700d6568a15bac6eed77316e60bdf4981f17e
david <davidventura27@gmail.com>
2021-01-03 20:10:46 +0100

    Workers code

--- kindle-events-parser/src/main.rs
+++ kindle-events-parser/src/main.rs
@@ -1,3 +1,4 @@
+use libopenlipc_sys::rLIPC;
 use phf::phf_map;
 use std::io::{BufRead, BufReader};
 use std::process::{Command, Stdio};
@@ -9,6 +10,7 @@ use std::thread::spawn;
 enum Event {
     Connected,
     ScreenOff,
+    ScreenOn,
     BatteryChanged,
 }
 
@@ -18,18 +20,39 @@ struct EventData {
     data: String,
 }
 
+#[derive(Debug)]
+struct EventFilter<'a> {
+    source: &'a str,
+    events: Vec<&'a str>,
+}
+
 static MAP: phf::Map<&'static str, Event> = phf_map! {
-    "cmStateChange \"CONNECTED\"" => Event::Connected,
+    "cmConnected" => Event::Connected,
     "goingToScreenSaver" => Event::ScreenOff,
+    "outOfScreenSaver" => Event::ScreenOn,
     "battLevelChanged" => Event::BatteryChanged,
 };
 
-fn run_and_match(event_source: &str, event_name: &str, tx: Sender<EventData>) {
+/*
+static EVENT_TO_TOPIC: phf::Map<Event, &'static str> = phf_map! {
+    Event::Connected => "KINDLE/CONNECTED",
+    Event::ScreenOn => "KINDLE/SCREEN_STATE",
+    Event::ScreenOff => "KINDLE/SCREEN_STATE",
+ => "KINDLE/CONNECTED",
+};
+
+*/
+#[allow(dead_code)]
+#[allow(unused_variables)]
+fn publish(topic: &str, value: &str) {}
+
+fn run_and_match(filter: EventFilter, tx: Sender<EventData>) {
+    // event_source: &str, event_name: &str
     let mut c = Command::new("lipc-wait-event")
-        .args(vec!["-m", event_source, event_name])
+        .args(vec!["-m", filter.source, filter.events.join(",").as_str()])
         .stdout(Stdio::piped())
         .spawn()
-        .unwrap();
+        .expect(format!("Could not start lipc-wait-event with filter {:?}", filter).as_str());
 
     let stdout = c.stdout.as_mut().unwrap();
     let reader = BufReader::new(stdout);
@@ -39,7 +62,7 @@ fn run_and_match(event_source: &str, event_name: &str, tx: Sender<EventData>) {
             continue;
         }
         let line = result.unwrap();
-        println!("[{}] {}", event_source, line);
+        println!("[{}] {}", filter.source, line);
         for k in MAP.keys().into_iter() {
             if line.starts_with(k) {
                 let event = MAP.get(k).unwrap().clone();
@@ -57,17 +80,23 @@ fn main() {
     println!("Started!");
     let (tx, rx): (Sender<EventData>, Receiver<EventData>) = mpsc::channel();
 
-    let power_tx = tx.clone();
-    let wifi_tx = tx.clone();
+    let mut workers = Vec::new();
 
-    let power = spawn(|| {
-        run_and_match(
-            "com.lab126.powerd",
-            "goingToScreenSaver,battLevelChanged",
-            power_tx,
-        )
-    });
-    let wifi = spawn(|| run_and_match("com.lab126.wifid", "cmStateChange", wifi_tx));
+    let r = rLIPC::new().unwrap();
+
+    for filter in vec![
+        EventFilter {
+            source: "com.lab126.powerd",
+            events: vec!["goingToScreenSaver", "battLevelChanged"],
+        },
+        EventFilter {
+            source: "com.lab126.wifid",
+            events: vec!["cmConnected"],
+        },
+    ] {
+        let _tx = tx.clone();
+        workers.push(spawn(|| run_and_match(filter, _tx)));
+    }
 
     let event_parser = spawn(move || loop {
         let rcv = rx.recv().unwrap();
@@ -75,17 +104,17 @@ fn main() {
             (Event::BatteryChanged, data) => {
                 // "battLevelChanged <> " where <> is u32 always -> i32 (using -1 to indicate some
                 // unexpected error?)
-                let battery_status: i32 =
-                    data.split(" ").nth(1).unwrap_or("-1").parse().unwrap_or(-1);
+                let battery_status = data.split(" ").nth(1).unwrap_or("-1").parse().unwrap_or(-1);
                 println!("Battery at {}%", battery_status);
             }
             (Event::Connected, _) => println!("Wifi Connected"),
+            (Event::ScreenOn, _) => println!("Screen on"),
             (Event::ScreenOff, _) => println!("Screen off"),
         }
     });
 
-    event_parser.join().unwrap();
-    println!("Done receiving");
-    wifi.join().unwrap();
-    power.join().unwrap();
+    workers.push(event_parser);
+    for w in workers {
+        w.join().expect("Job died!");
+    }
 }
