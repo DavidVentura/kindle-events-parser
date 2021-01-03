f32ac6f62c25b70130363d8aad50e504f23af4a1
david <davidventura27@gmail.com>
2021-01-03 19:41:00 +0100

    Use env variable for build

--- libopenlipc-sys/build.rs
+++ libopenlipc-sys/build.rs
@@ -1,6 +1,9 @@
+use std::env;
 fn main() {
+    let sysroot_lib_dir = env::var("SYSROOT_LIB_DIR").expect("Set SYSROOT_LIB_DIR pointing to Amazon-Kindle-Cross-Toolchain/arm-kindle-linux-gnueabi/arm-kindle-linux-gnueabi/sysroot/lib/");
+
     println!("cargo:rustc-link-search=so");
-    println!("cargo:rustc-link-search=/home/david/git/Amazon-Kindle-Cross-Toolchain/arm-kindle-linux-gnueabi/arm-kindle-linux-gnueabi/sysroot/lib/");
+    println!("cargo:rustc-link-search={}", sysroot_lib_dir);
     println!("cargo:rustc-link-lib=glib-2.0");
     println!("cargo:rustc-link-lib=dbus-1");
     println!("cargo:rustc-link-lib=gthread-2.0");
